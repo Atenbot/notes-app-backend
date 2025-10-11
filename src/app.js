362c4518106978da8c2ff43e7949f6a00e13e7cc
atenbot <aten27sun@gmail.com>
2025-10-11 21:42:47 +0100

    allow localhost url

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? process.env.FRONTEND_URL
+      ? [process.env.FRONTEND_URL, "http://localhost:5173"]
       : ["http://localhost:5173", "http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
