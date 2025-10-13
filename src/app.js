9d7504946bc88c37d3c18bb1e3e9fe94e16fd21b
atenbot <aten27sun@gmail.com>
2025-10-13 20:16:44 +0100

    allow all origins to test CORS

--- src/app.js
+++ src/app.js
@@ -13,10 +13,10 @@ const notesRoutes = require("./routes/notes");
 const app = express();
 
 const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? [process.env.FRONTEND_URL, "http://localhost:5173"]
-      : ["http://localhost:5173", "http://localhost:3000"],
+  origin: "*",
+  // process.env.NODE_ENV === "production"
+  //   ? [process.env.FRONTEND_URL, "http://localhost:5173"]
+  //   : ["http://localhost:5173", "http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
