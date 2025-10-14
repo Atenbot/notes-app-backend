231382c67d42f9af2f336405fd13b7cdfbd3c7fa
atenbot <aten27sun@gmail.com>
2025-10-14 21:33:09 +0100

    updated google callback url

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL:
+        "https://notes-app-backend-production-ef0d.up.railway.app/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
