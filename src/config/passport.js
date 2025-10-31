c185e82cae893b4c544f2c7358a8d573bdc65905
atenbot <aten27sun@gmail.com>
2025-10-31 21:05:26 +0800

    migrated to koyeb

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        "https://notes-app-backend-production-ef0d.up.railway.app/api/auth/google/callback",
+        "https://small-orelie-aten-0815d59a.koyeb.app/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
